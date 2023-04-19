e9e4beb6beea54a6403260c2d3b41e577568c338
Carl Meyertons <cmeyertons@servicetitan.com>
2023-04-19 11:46:34 -0500

    feat: CancellationToken support for AsyncCursorSource

--- src/MongoAsyncEnumerableAdapter/AsyncCursorSourceExtensions.cs
+++ src/MongoAsyncEnumerableAdapter/AsyncCursorSourceExtensions.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
-using MongoDB.Driver;
+using System.Threading;
 
 namespace MongoAsyncEnumerableAdapter
 {
@@ -18,9 +19,22 @@ namespace MongoAsyncEnumerableAdapter
         public static IAsyncEnumerable<TDocument> ToAsyncEnumerable<TDocument>(
             this IAsyncCursorSource<TDocument> source)
         {
+            return source.ToAsyncEnumerable(CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Wraps a cursor source in an <see cref="IAsyncEnumerable{T}" />. Each time <see cref="IAsyncEnumerable{T}.GetAsyncEnumerator" /> is called a new cursor is fetched from the cursor source.
+        /// </summary>
+        /// <typeparam name="TDocument">The type of the document.</typeparam>
+        /// <param name="source">The source.</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>An <see cref="IAsyncEnumerable{T}" />.</returns>
+        public static IAsyncEnumerable<TDocument> ToAsyncEnumerable<TDocument>(
+            this IAsyncCursorSource<TDocument> source, CancellationToken cancellationToken)
+        {
             if (source is null) throw new ArgumentNullException(nameof(source));
 
-            return AsyncCursorExtensions.ToAsyncEnumerable(source, cursor: null);
+            return AsyncCursorExtensions.ToAsyncEnumerable(source, cursor: null, cancellationToken: cancellationToken);
         }
     }
 }
